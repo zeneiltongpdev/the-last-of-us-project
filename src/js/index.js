c43a9e512b98235f7eea0ab8595fae0b77c3912d
✞Ȥɘɳɘʅ✞ <zeneiltongranja@gmail.com>
2023-03-11 21:55:49 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const botesCarrossel = document.querySelectorAll(".botao");
+const imagens = document.querySelectorAll(".imagem");
+
+botesCarrossel.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desativarBotaoSelecionado();
+
+    selecionarBotaoCarrossel(botao);
+
+    esconderImagemAtiva();
+
+    mostrarImagemAtiva(indice);
+  });
+});
+
+function mostrarImagemAtiva(indice) {
+  imagens[indice].classList.add("ativa");
+}
+
+function selecionarBotaoCarrossel(botao) {
+  botao.classList.add("selecionado");
+}
+
+function esconderImagemAtiva() {
+  const imagemAtiva = document.querySelector(".ativa");
+  imagemAtiva.classList.remove("ativa");
+}
+
+function desativarBotaoSelecionado() {
+  const botaoSelecionado = document.querySelector(".selecionado");
+  botaoSelecionado.classList.remove("selecionado");
+}
